da9095d893febca7a097cf2771f2612c49d96d70
Ammer97 <ameerwisam97@gmail.com>
2019-05-01 14:46:08 +0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToOrderColumns = True
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(990, 247)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 317)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(161, 51)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Run"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(77, 265)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(77, 291)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 268)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 294)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "password"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(321, 345)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(162, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(461, 319)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(168, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(321, 318)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(134, 21)
+        Me.ComboBox1.TabIndex = 9
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(321, 261)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(308, 52)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Activate"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(489, 345)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(140, 23)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "delete"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Button3
+        Me.ClientSize = New System.Drawing.Size(1011, 389)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,159 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Dim flag As Boolean = False
+    Dim dt As DataTable = New DataTable()
+    Dim view As DataView
+
+    Private Sub getXlFile()
+
+
+        OpenFileDialog1.Filter = "Excel 2003 Documents (*.xls) | *.xls"
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim fileName As String
+            fileName = OpenFileDialog1.FileName
+            getExcel(fileName)
+        End If
+
+    End Sub
+
+    Private Sub getExcel(openFDFilename As String)
+
+        Dim s As String = ""
+        Dim connectionString = String.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=""Excel 8.0; HDR = Yes; IMEX = 1""", openFDFilename)
+
+        Dim adapter As OleDbDataAdapter
+        adapter = New OleDbDataAdapter("select * from [patient$]", connectionString)
+
+        Dim Data = New DataTable()
+
+        Dim ds = New DataSet()
+        adapter.Fill(ds, "patient")
+        Data = ds.Tables("patient")
+        dt = Data
+        DataGridView1.DataSource = Encrypter(Data)
+
+    End Sub
+    Function Encrypter(DataTableInstance As DataTable)
+
+        If flag Then
+            Return DataTableInstance
+        End If
+        For i = 0 To Convert.ToInt32(DataTableInstance.Rows.Count - 1)
+            For j = 0 To Convert.ToInt32(DataTableInstance.Columns.Count - 1)
+                DataTableInstance.Rows(i).Item(j) = Encrypt(DataTableInstance.Rows(i).Item(j))
+            Next
+        Next
+        Return DataTableInstance
+    End Function
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        'Dim path = "C:\Users\Ameer\Desktop\phpkey.txt"
+        'Dim check As IO.StreamReader = New IO.StreamReader(path)
+        'Dim checkstring = check.ReadToEnd
+        Dim username = TextBox1.Text
+        Dim password = TextBox2.Text
+
+        If username = "doctor" AndAlso password = "1234" Then
+            flag = True
+            MessageBox.Show("You Are a Doctor")
+        Else
+            MessageBox.Show("You are not a Doctor")
+            flag = False
+
+        End If
+        Dim watch As Stopwatch = Stopwatch.StartNew()
+        watch.Start()
+        getXlFile()
+        watch.Stop()
+        MessageBox.Show(watch.Elapsed.TotalSeconds)
+
+        If flag Then
+            Button3.Enabled = True
+        End If
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim dt As DataTable = New DataTable()
+        For Each col As DataGridViewColumn In DataGridView1.Columns
+            dt.Columns.Add(col.Name)
+        Next
+
+        For Each row As DataGridViewRow In DataGridView1.Rows
+            Dim dRow As DataRow = dt.NewRow()
+
+            For Each cell As DataGridViewCell In row.Cells
+                dRow(cell.ColumnIndex) = cell.Value
+            Next
+
+            dt.Rows.Add(dRow)
+        Next
+        Dim customerTable As DataSet = New DataSet()
+        dt.TableName = "patient"
+        customerTable.Tables.Add(dt)
+        ExcelLibrary.DataSetHelper.CreateWorkbook(OpenFileDialog1.FileName, customerTable)
+        MessageBox.Show("Your file has been saved")
+    End Sub
+
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        Dim query = _
+       From order In dt.AsEnumerable() _
+       Where order.Field(Of String)(ComboBox1.SelectedItem.ToString).Contains(TextBox3.Text)
+       Select order
+
+        view = query.AsDataView()
+        DataGridView1.DataSource = view
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button2.Enabled = False
+        ComboBox1.Enabled = False
+        TextBox3.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim activate As String
+        If flag Then
+            activate = InputBox("Enter Activate Code")
+        End If
+        If activate = "a" Then
+            For Each column In dt.Columns
+                ComboBox1.Items.Add(column.ColumnName)
+            Next
+            ComboBox1.SelectedIndex = 0
+            Button2.Enabled = True
+            ComboBox1.Enabled = True
+            TextBox3.Enabled = True
+            Button4.Enabled = True
+        Else
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim selected = DataGridView1.SelectedRows.Count
+        If selected > 0 Then
+            Dim result = MessageBox.Show("Do you want to delete these " & selected & " Rows", "caption", MessageBoxButtons.YesNo)
+            Select Case result
+                Case 6 ' i.e "yes"
+                    For Each row As DataGridViewRow In DataGridView1.SelectedRows
+                        DataGridView1.Rows.Remove(row)
+                    Next
+                Case 7 ' i.e "no"
+                    MessageBox.Show("No any row has been deleted")
+            End Select
+        End If
+        MessageBox.Show("Susuessfilly deleted these " & selected & " Rows")
+
+    End Sub
+End Class
